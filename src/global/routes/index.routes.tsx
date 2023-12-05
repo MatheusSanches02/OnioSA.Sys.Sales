0e691e1cecf5fe4e40ec4cf1122355afa0361e36
Matheus Sanches <matheus.sanches@kria.com.br>
2023-12-05 15:58:25 -0300

    ajuste no roteamento

--- src/global/routes/index.routes.tsx
+++ src/global/routes/index.routes.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "../../pages/home/index.home";
+import Upload from "../../pages/uploadFiles/index.upload";
 
 export default function AppRoutes() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/home/uploadFiles" element={<Home />} />
+          <Route path="/home" element={<Home />}>
+            <Route path="/home/uploadFiles" element={<Upload />}></Route>
+          </Route>
         </Routes>
       </Router>
     </>
--- src/pages/home/index.home.tsx
+++ src/pages/home/index.home.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -21,6 +22,9 @@ export default function Home() {
         theme="dark"
       />
       <header className="w-full bg-red-600 h-full"></header>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
